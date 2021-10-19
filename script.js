522ff9fd522a5028edaacd4c2020323cd6388ef3
Brian Helms <bhelms2132@gmail.com>
2021-10-18 21:16:59 -0400

    async/await function created and working properly

--- script.js
+++ script.js
@@ -7,8 +7,22 @@ jokeBtn.addEventListener('click', generateJoke)
 // Calls function to be run
 generateJoke()
 
+// Using async/await
+async function generateJoke(params) {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
 
-function generateJoke(params) {
+    const res = await fetch('https://icanhazdadjoke.com', config)
+      const data = await res.json()
+      jokeEl.innerHTML = data.joke
+      
+}
+
+// Using .then()
+/* function generateJoke(params) {
     const config = {
         headers: {
             'Accept': 'application/json'
@@ -19,4 +33,4 @@ function generateJoke(params) {
       .then((data) => {
           jokeEl.innerHTML = data.joke
       })
-}
+}*/
